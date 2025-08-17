07ffc807fae71df305aa2e8ac2efec138ddbc184
Byte <bytehash69@gmail.com>
2025-08-18 01:16:22 +0530

    Ix-Deposit & Withdraw -- Tests-working

--- programs/anchor-vault/src/lib.rs
+++ programs/anchor-vault/src/lib.rs
@@ -4,13 +4,53 @@ declare_id!("B8nJgmX8ZqbTbmp9UakSihNbCWbDocxFDjfMqjQ5hUnW");
 
 #[program]
 pub mod anchor_vault {
+
+    use anchor_lang::system_program::{self, Transfer};
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn deposit(ctx: Context<VaultAction>, amount: u64) -> Result<()> {
+        system_program::transfer(
+            CpiContext::new(
+                ctx.accounts.system_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.user.to_account_info(),
+                    to: ctx.accounts.vault.to_account_info(),
+                },
+            ),
+            amount,
+        )?;
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<VaultAction>, amount: u64) -> Result<()> {
+        let seeds: &[&[&[u8]]] = &[&[b"vault", ctx.accounts.user.key.as_ref(), &[ctx.bumps.vault]]];
+        system_program::transfer(
+            CpiContext::new_with_signer(
+                ctx.accounts.system_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.vault.to_account_info(),
+                    to: ctx.accounts.user.to_account_info(),
+                },
+                seeds,
+            ),
+            amount,
+        )?;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct VaultAction<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"vault", user.key().as_ref()],
+        bump
+    )]
+    pub vault: SystemAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+}
--- tests/anchor-vault.ts
+++ tests/anchor-vault.ts
@@ -1,16 +1,37 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorVault } from "../target/types/anchor_vault";
+import { Keypair, PublicKey } from "@solana/web3.js";
+import { expect } from "chai";
 
 describe("anchor-vault", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
   const program = anchor.workspace.anchorVault as Program<AnchorVault>;
+  
+  const user = provider.wallet.payer;
+  const [vault, bump] = PublicKey.findProgramAddressSync([Buffer.from("vault"), user.publicKey.toBuffer()], program.programId)
+  const amount = new anchor.BN(1_000_000_000)
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  it("Transfer amount to vault", async () => {
+
+
+    const tx = await program.methods.deposit(amount).accounts({user: user.publicKey, vault: vault}).signers([user]).rpc();
+
+    const vaultBalance = await provider.connection.getBalance(vault);
+
+    expect(vaultBalance).to.be.eq(amount.toNumber());
+    console.log("Your transaction signature", tx);
+  });
+
+
+  it("Withdraw vault amount", async () => {
+    const tx = await program.methods.withdraw(amount).accounts({user: user.publicKey, vault: vault}).rpc();
+
+    const vaultBalance = await provider.connection.getBalance(vault);
+
+    expect(vaultBalance).to.be.eq(0);
     console.log("Your transaction signature", tx);
   });
 });
