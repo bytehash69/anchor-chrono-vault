01c255521bea6a6ddd8c2c0ab577857f6152bb46
Byte <bytehash69@gmail.com>
2025-08-18 18:29:41 +0530

    Modified the contract to perform as chrono vault

--- programs/anchor-vault/src/lib.rs
+++ programs/anchor-vault/src/lib.rs
@@ -1,43 +1,114 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{transfer, Mint, Token, TokenAccount, Transfer},
+};
 
 declare_id!("B8nJgmX8ZqbTbmp9UakSihNbCWbDocxFDjfMqjQ5hUnW");
 
 #[program]
 pub mod anchor_vault {
 
-    use anchor_lang::system_program::{self, Transfer};
-
     use super::*;
 
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        lock_duration: u64,
+        token_mint: Pubkey,
+    ) -> Result<()> {
+        let clock = Clock::get()?;
+        let current_time = clock.unix_timestamp as u64;
+        let unlock_time = current_time + lock_duration;
+
+        ctx.accounts.chrono_account.set_inner(Chrono {
+            deposit_time: current_time,
+            unlock_time: unlock_time,
+            mint: token_mint.key(),
+        });
+
+        Ok(())
+    }
+
     pub fn deposit(ctx: Context<VaultAction>, amount: u64) -> Result<()> {
-        system_program::transfer(
-            CpiContext::new(
-                ctx.accounts.system_program.to_account_info(),
-                Transfer {
-                    from: ctx.accounts.user.to_account_info(),
-                    to: ctx.accounts.vault.to_account_info(),
-                },
-            ),
-            amount,
-        )?;
+        let cpi_ctx = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            Transfer {
+                authority: ctx.accounts.user.to_account_info(),
+                from: ctx.accounts.user_ata.to_account_info(),
+                to: ctx.accounts.vault.to_account_info(),
+            },
+        );
+
+        transfer(cpi_ctx, amount)?;
         Ok(())
     }
 
     pub fn withdraw(ctx: Context<VaultAction>, amount: u64) -> Result<()> {
-        let seeds: &[&[&[u8]]] = &[&[b"vault", ctx.accounts.user.key.as_ref(), &[ctx.bumps.vault]]];
-        system_program::transfer(
-            CpiContext::new_with_signer(
-                ctx.accounts.system_program.to_account_info(),
-                Transfer {
-                    from: ctx.accounts.vault.to_account_info(),
-                    to: ctx.accounts.user.to_account_info(),
-                },
-                seeds,
-            ),
-            amount,
-        )?;
+        let clock = Clock::get()?;
+        let current_time = clock.unix_timestamp as u64;
+
+        require!(
+            current_time >= ctx.accounts.chrono_account.unlock_time,
+            Errors::TokensStillLocked
+        );
+
+        let seeds: &[&[&[u8]]] = &[&[
+            b"chrono",
+            ctx.accounts.user.key.as_ref(),
+            &[ctx.bumps.chrono_account],
+        ]];
+
+        let cpi_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            Transfer {
+                authority: ctx.accounts.chrono_account.to_account_info(),
+                from: ctx.accounts.vault.to_account_info(),
+                to: ctx.accounts.user_ata.to_account_info(),
+            },
+            seeds,
+        );
+
+        transfer(cpi_ctx, amount)?;
         Ok(())
     }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub mint: Account<'info, Mint>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = user,
+        associated_token::token_program = token_program
+    )]
+    pub user_ata: Account<'info, TokenAccount>,
+
+    #[account(
+        init,
+        payer = user,
+        space = 8 + Chrono::INIT_SPACE,
+        seeds = [b"chrono", user.key().as_ref()],
+        bump
+    )]
+    pub chrono_account: Account<'info, Chrono>,
+
+    #[account(
+        init_if_needed,
+        payer = user,
+        associated_token::mint = mint,
+        associated_token::authority = chrono_account,
+        associated_token::token_program = token_program
+    )]
+    pub vault: Account<'info, TokenAccount>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
 }
 
 #[derive(Accounts)]
@@ -45,12 +116,47 @@ pub struct VaultAction<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
 
+    pub mint: Account<'info, Mint>,
+
     #[account(
         mut,
-        seeds = [b"vault", user.key().as_ref()],
+        associated_token::mint = mint,
+        associated_token::authority = user,
+        associated_token::token_program = token_program
+    )]
+    pub user_ata: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        seeds = [b"chrono", user.key().as_ref()],
         bump
     )]
-    pub vault: SystemAccount<'info>,
+    pub chrono_account: Account<'info, Chrono>,
+
+    #[account(
+        init_if_needed,
+        payer = user,
+        associated_token::mint = mint,
+        associated_token::authority = chrono_account,
+        associated_token::token_program = token_program
+    )]
+    pub vault: Account<'info, TokenAccount>,
 
     pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Chrono {
+    deposit_time: u64,
+    unlock_time: u64,
+    mint: Pubkey,
+}
+
+#[error_code]
+pub enum Errors {
+    #[msg("Lock time is not completed yet")]
+    TokensStillLocked,
 }
--- tests/anchor-vault.ts
+++ tests/anchor-vault.ts
@@ -1,37 +1,156 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorVault } from "../target/types/anchor_vault";
-import { Keypair, PublicKey } from "@solana/web3.js";
+import {
+  Keypair,
+  PublicKey,
+} from "@solana/web3.js";
 import { expect } from "chai";
+import {
+  createMint,
+  getAccount,
+  getAssociatedTokenAddressSync,
+  getOrCreateAssociatedTokenAccount,
+  mintTo,
+} from "@solana/spl-token";
 
 describe("anchor-vault", () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
   const program = anchor.workspace.anchorVault as Program<AnchorVault>;
-  
+
   const user = provider.wallet.payer;
-  const [vault, bump] = PublicKey.findProgramAddressSync([Buffer.from("vault"), user.publicKey.toBuffer()], program.programId)
-  const amount = new anchor.BN(1_000_000_000)
+  const connection = provider.connection;
 
-  it("Transfer amount to vault", async () => {
+  const amount = new anchor.BN(1_000_000_000);
 
+  let mint: PublicKey;
+  let userTokenAccount: PublicKey;
+  let chrono: PublicKey;
+  let vault: PublicKey;
 
-    const tx = await program.methods.deposit(amount).accounts({user: user.publicKey, vault: vault}).signers([user]).rpc();
+  before("Create new token mint and mint tokens", async () => {
+    // create new mint
+    mint = await createMint(
+      connection,             // connection
+      user,                   // fee payer
+      user.publicKey,         // mint authority
+      null,                   // freeze authority (none)
+      9                       // decimals
+    );
+    console.log("Mint address:", mint.toBase58());
 
-    const vaultBalance = await provider.connection.getBalance(vault);
+    // create ATA for user
+    const ata = await getOrCreateAssociatedTokenAccount(
+      connection,
+      user,
+      mint,
+      user.publicKey
+    );
+    userTokenAccount = ata.address;
+    console.log("User ATA:", userTokenAccount.toBase58());
 
-    expect(vaultBalance).to.be.eq(amount.toNumber());
-    console.log("Your transaction signature", tx);
+    // mint tokens to user ATA
+    await mintTo(
+      connection,
+      user,
+      mint,
+      userTokenAccount,
+      user, // authority
+      5_000_000_000
+    );
+
+    const accountInfo = await getAccount(connection, userTokenAccount);
+    console.log("User token balance:", accountInfo.amount.toString());
+    expect(Number(accountInfo.amount)).to.eq(5_000_000_000);
+
+    // derive chrono PDA
+    [chrono] = PublicKey.findProgramAddressSync(
+      [Buffer.from("chrono"), user.publicKey.toBuffer()],
+      program.programId
+    );
+
+    // derive vault ATA owned by chrono PDA
+    vault = getAssociatedTokenAddressSync(mint, chrono, true);
+    console.log("Vault ATA:", vault.toBase58());
   });
 
+  it("Initialized vault", async () => {
+    const lockDuration = new anchor.BN(5);
 
-  it("Withdraw vault amount", async () => {
-    const tx = await program.methods.withdraw(amount).accounts({user: user.publicKey, vault: vault}).rpc();
+    const tx = await program.methods
+      .initialize(lockDuration, mint)
+      .accounts({
+        user: user.publicKey,
+        mint: mint,
+        userAta: userTokenAccount,
+        chronoAccount: chrono,
+        vault: vault,
+      })
+      .rpc();
 
-    const vaultBalance = await provider.connection.getBalance(vault);
+    console.log("Transaction signature (initialize): ", tx);
+  });
 
-    expect(vaultBalance).to.be.eq(0);
-    console.log("Your transaction signature", tx);
+  it("Transfer amount to vault", async () => {
+    const tx = await program.methods
+      .deposit(amount)
+      .accounts({
+        user: user.publicKey,
+        mint: mint,
+        userAta: userTokenAccount,
+        chronoAccount: chrono,
+        vault: vault,
+      })
+      .rpc();
+
+    const vaultAccount = await getAccount(connection, vault);
+    console.log("Vault balance:", vaultAccount.amount.toString());
+    console.log("Transaction signature (deposit): ", tx);
+
+    expect(Number(vaultAccount.amount)).to.eq(1_000_000_000);
+  });
+
+  it("Cannot withdraw when vault is locked", async() => {
+    try {
+      const tx = await program.methods
+        .withdraw(amount)
+        .accounts({
+          user: user.publicKey,
+          mint: mint,
+          userAta: userTokenAccount,
+          chronoAccount: chrono,
+          vault: vault,
+      })
+      .rpc();
+      expect.fail("Should fail cause vault is locked yet");
+    } catch (error) {
+      console.log("Withdrawal rejected before unlock time")
+    }
+  })
+
+  it("Transfer amount to vault after vault unlocks", async () => {
+
+    await new Promise((res:any) => {
+      setTimeout(()=>{
+        res("");
+      }, 6000)
+    })
+
+    const tx = await program.methods
+    .withdraw(amount)
+    .accounts({
+        user: user.publicKey,
+        mint: mint,
+        userAta: userTokenAccount,
+        chronoAccount: chrono,
+        vault: vault,
+      })
+      .rpc();
+
+    const userAta = await getAccount(connection, userTokenAccount);
+    console.log("User ATA balance:", userAta.amount.toString());
+    console.log("Transaction signature (deposit): ", tx);
   });
 });
